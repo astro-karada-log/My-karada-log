ae0341ad9b46df7f055167cf2ba00ebe8226e6f3
astro-karada-log <neo_flyingace@yahoo.co.jp>
2026-08-15 03:03:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='karada-log-v1.9.7-yui-fix1';
+const CACHE='karada-log-v1.9.8-yui-fix1';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./manifest.json','./icon-180.png','./apple-touch-icon.png','./assets/yui/yui-good.png', './assets/yui/yui-aha.png', './assets/yui/yui-thinking.png', './assets/yui/yui-agree.png', './assets/yui/yui-cheer.png', './assets/yui/yui-hmm.png', './assets/yui/yui-oops.png', './assets/yui/yui-surprised.png', './assets/yui/yui-relieved.png', './assets/yui/yui-happy.png', './assets/yui/yui-idea.png', './assets/yui/yui-complete.png', './assets/yui/yui-working.png', './assets/yui/yui-check.png', './assets/yui/yui-together.png', './assets/yui/yui-thanks.png'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
